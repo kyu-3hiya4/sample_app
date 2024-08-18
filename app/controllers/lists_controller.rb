06f02557c5e6cd09fdcc4f0cc2a30f73d63ec852
kyu-3hiya4 <177545609+kyu-3hiya4>
2024-08-18 02:17:08 +0000

    [Finish] 3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,10 +5,12 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    list.save
-    # 詳細画面へリダイレクト
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def index
